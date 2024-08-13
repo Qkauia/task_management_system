1565ab04e963f4f7ccc71c709af805d4d769308c
SunEric <kauiaguy@gmail.com>
2024-08-13 21:24:15 +0800

    feat: Add admin role functionalities and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
-  before_save :encrypt_password
+  acts_as_paranoid
+
+  before_save :encrypt_password, if: -> { password.present? }
+  before_save :set_default_role
+
   has_many :tasks, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true, if: -> { password.present? }
 
   attr_accessor :password, :password_confirmation, :current_password
+
+  enum role: { user: 'user', admin: 'admin' }
 
   def authenticate(password)
     password_hash == encrypt(password)
@@ -14,13 +20,15 @@ class User < ApplicationRecord
   private
 
   def encrypt_password
-    return if password.blank?
-
     self.password_salt = SecureRandom.hex(16)
     self.password_hash = encrypt(password)
   end
 
   def encrypt(password)
     Digest::SHA2.hexdigest(password + password_salt)
+  end
+
+  def set_default_role
+    self.role ||= :user
   end
 end
