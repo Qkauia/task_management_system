1aedb709192d4e9977cfa3f752f36d25411c2543
SunEric <kauiaguy@gmail.com>
2024-08-13 21:21:39 +0800

    feat: Add User Table role and deleted_at cloumns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_12_072545) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_13_125810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_12_072545) do
     t.datetime "updated_at", null: false
     t.string "password_hash", null: false
     t.string "password_salt", null: false
+    t.string "role"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
