e9f956d36e5b0f86e0383eee43dda3d26d7e8df8
SunEric <kauiaguy@gmail.com>
2024-08-15 10:13:49 +0800

    tset: Update User FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,18 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    password { 'password' }
-    password_confirmation { 'password' }
+    password { "password" }
+    password_confirmation { "password" }
+    password_salt { SecureRandom.hex(16) }
+    password_hash { Digest::SHA2.hexdigest("password" + password_salt) }
+    role { 'user' }
+  end
+
+  trait :admin do
+    role { 'admin' }
+  end
+
+  trait :deleted do
+    deleted_at { Time.current }
   end
 end
