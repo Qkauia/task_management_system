8724040aed9c136b000211f5c76a00fb72e29586
SunEric <kauiaguy@gmail.com>
2024-08-16 14:10:53 +0800

    chore: Yarn init and add  bootstrap, esbuild, esbuild-sass-plugin and set run bin/dev

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import { Application } from "@hotwired/stimulus"
+import "./controllers"
+const application = Application.start()
+
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
