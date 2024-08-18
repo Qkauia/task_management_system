c05bf4648de7ed89523b3291076a07b21003b664
SunEric <kauiaguy@gmail.com>
2024-08-18 22:32:46 +0800

    feat: flatpickr setting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,15 @@ const application = Application.start()
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./controllers/tag_buttons_controller"
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Mandarin } from "flatpickr/dist/l10n/zh"
+
+document.addEventListener("turbo:load", () => {
+  flatpickr(".datetime-picker", {
+    locale: Mandarin,
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+  });
+});
