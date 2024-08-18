cec21a6abc5147e75a1cec19e6ae1bfefda82932
SunEric <kauiaguy@gmail.com>
2024-08-18 20:45:00 +0800

    feat: Add create_new_tag_if_needed method and add bootstrap style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import "@hotwired/turbo-rails"
 import "./controllers"
 const application = Application.start()
 
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./controllers/tag_buttons_controller"
