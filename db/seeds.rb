589bce03dc433fcbe330a7095034b1846652936c
SunEric <kauiaguy@gmail.com>
2024-08-19 23:16:27 +0800

    fix: rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 # Create Tags
 tags = 10.times.map do
   tag = Tag.create!(name: Faker::Hobby.activity)
-  puts "Created Tag: #{tag.name}"
+  Rails.logger.debug { "Created Tag: #{tag.name}" }
   tag
 end
 
@@ -22,7 +22,7 @@ end
     password_confirmation: 'password',
     role: 'admin'
   )
-  puts "Created Admin User: #{user.email}"
+  Rails.logger.debug { "Created Admin User: #{user.email}" }
 
   # 每位 admin 使用者建立 15 筆任務
   15.times do
@@ -39,7 +39,7 @@ end
     # 隨機分配標籤
     assigned_tags = tags.sample(rand(1..3))
     task.tags << assigned_tags
-    puts "Created Task: #{task.title} for Admin User: #{user.email} with Tags: #{assigned_tags.map(&:name).join(', ')}"
+    Rails.logger.debug { "Created Task: #{task.title} for Admin User: #{user.email} with Tags: #{assigned_tags.map(&:name).join(', ')}" }
   end
 end
 
@@ -51,7 +51,7 @@ end
     password_confirmation: 'password',
     role: 'user'
   )
-  puts "Created Regular User: #{user.email}"
+  Rails.logger.debug { "Created Regular User: #{user.email}" }
 
   # 每位普通使用者建立 15 筆任務
   15.times do
@@ -68,8 +68,8 @@ end
     # 隨機分配標籤
     assigned_tags = tags.sample(rand(1..3))
     task.tags << assigned_tags
-    puts "Created Task: #{task.title} for Regular User: #{user.email} with Tags: #{assigned_tags.map(&:name).join(', ')}"
+    Rails.logger.debug { "Created Task: #{task.title} for Regular User: #{user.email} with Tags: #{assigned_tags.map(&:name).join(', ')}" }
   end
 end
 
-puts "Seed data created successfully!"
+Rails.logger.debug "Seed data created successfully!"
