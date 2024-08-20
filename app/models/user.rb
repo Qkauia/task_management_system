3d7923a76e875f0342ede28d5bc647cc4d0c0ad4
SunEric <kauiaguy@gmail.com>
2024-08-20 14:08:05 +0800

    feat: Add a new relationship between user and notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   before_save :set_default_role
 
   has_many :tasks, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true, if: -> { password.present? }
