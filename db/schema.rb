3a865c286e9d7c0988f173b4122e0f5f44392ef2
SunEric <kauiaguy@gmail.com>
2024-08-20 16:43:44 +0800

    data: add avatar column to User Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_19_160230) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_20_084240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_19_160230) do
     t.string "password_salt", null: false
     t.string "role"
     t.datetime "deleted_at"
+    t.string "avatar"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
