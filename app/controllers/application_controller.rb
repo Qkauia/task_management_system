d4f2f815baed43273e0fa1f823974157799f1218
SunEric <kauiaguy@gmail.com>
2024-08-20 15:55:37 +0800

    feat: Notifications setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   helper_method :current_user
   before_action :set_locale
+  before_action :set_notifications
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -19,11 +20,28 @@ class ApplicationController < ActionController::Base
     redirect_to login_path unless current_user
   end
 
+  def check_task_notifications(user)
+    user.tasks.each do |task|
+      if task.end_time <= Time.current && user.notifications.where(task: task).blank?
+        user.notifications.create(task: task, message: "〖 #{task.title} 〗" + t('notification.task_end_time_has_passed').to_s)
+      elsif (task.end_time - 1.day) <= Time.current && user.notifications.where(task: task).blank?
+        user.notifications.create(task: task, message: "〖 #{task.title} 〗" + t('notification.task_is_due_soon').to_s)
+      end
+    end
+  end
+
   private
 
   def not_found
     render file: Rails.public_path.join('404.html'),
            status: :not_found,
            layout: false
+  end
+
+  def set_notifications
+    if current_user
+      @notifications = current_user.notifications.unread.includes(:task).limit(6)
+      @unread_count = @notifications.size
+    end
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
-  belongs_to :task
+  belongs_to :task, optional: true 
+
+  scope :unread, -> { where(read_at: nil) }
+  scope :old, ->(days = 7) { where(created_at: ...(Time.current - days.days)) }
 end
