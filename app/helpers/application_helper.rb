11c4a56eb30ddcff6b6b30fe322d7f517df4fa65
SunEric <kauiaguy@gmail.com>
2024-08-23 01:47:24 +0800

    fix: rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def sort_link(column, title)
     direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
     active_class = column == params[:sort] ? "active" : ""
-  
+
     link_to request.params.merge(sort: column, direction: direction), class: "text-decoration-none #{active_class}" do
-      content_tag(:span, title) + tag(:i, class: "icon-class")
+      content_tag(:span, title) + tag.i(class: "icon-class")
     end
   end
 end
