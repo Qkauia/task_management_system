d12b95449195e0d2dad7e68eb5bdc2107ae5710a
SunEric <kauiaguy@gmail.com>
2024-08-22 17:18:26 +0800

    chore: Yran add fullcalendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,30 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|zh-TW/ do
-    resources :tasks
     resources :users, only: [:new, :create, :edit, :update]
     namespace :admin do
       resources :users, only: [:index, :edit, :update, :destroy]
     end
-
-    get '/profile/edit', to: 'users#edit_profile', as: :edit_profile
-    patch '/profile', to: 'users#update_profile', as: :update_profile
+    resources :tasks
 
     resources :notifications, only: [:index] do
       member do
         patch :mark_as_read
       end
     end
-
+    
     resources :groups, except: [:show] do
       delete 'remove_user', on: :member
     end
-
+    
     root "tasks#index"
-
+    
     get 'signup', to: 'users#new'
     post 'signup', to: 'users#create'
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
+    get '/profile/edit', to: 'users#edit_profile', as: :edit_profile
+    patch '/profile', to: 'users#update_profile', as: :update_profile
   end
   get '', to: redirect("/%{locale}"), defaults: { locale: I18n.default_locale }
 end
