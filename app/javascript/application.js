99c8d32a931f14473a97233813bcdda0d135ac80
SunEric <kauiaguy@gmail.com>
2024-08-23 17:59:36 +0800

    feat: Add button in task form And sweetalert to dispaly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,20 @@
+import "@hotwired/turbo-rails";
+import "./controllers";
 import $ from 'jquery';
 window.$ = $;
 window.jQuery = $;
 import '../assets/stylesheets/application.css'
 
-import "@hotwired/turbo-rails";
-import "./controllers";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "flatpickr/dist/flatpickr.min.css";
 import 'select2';
 import 'select2/dist/css/select2.css';
+import "chart.js/auto";
+
 import './select2_flatpickr';
 import './show_calendar';
 import './sortable_tasks';
+import './tags_reports';
 import './task_sortablejs_operation_tips';
