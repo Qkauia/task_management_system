d0c54ad0dfb525aed40cf87b789c2efed851c7ce
SunEric <kauiaguy@gmail.com>
2024-08-29 23:53:31 +0800

    Remove: flatpickr stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,6 @@ import { application } from "./application"
 import TagButtonsController from "./tag_buttons_controller";
 application.register("tag-buttons", TagButtonsController);
 
-import TaskTimeController from "./task_time_controller";
-application.register("task-time", TaskTimeController);
-
 import UserButtonsController from "./user_buttons_controller";
 application.register("user-buttons", UserButtonsController);
 
